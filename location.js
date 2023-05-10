07030188d577c8c531d9d1faa4f4308eb0736165
Tirthaj Rikame <55919946+Tirthaj@users.noreply.github.com>
2023-05-10 14:09:30 -0400

    Update location.js

--- location.js
+++ location.js
@@ -3,9 +3,8 @@ if (navigator.geolocation) {
       var coordinates = document.getElementById('coordinates');
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
-      alert(`Got first GPS position: lon ${longitude} lat ${latitude}`);
       coordinates.setAttribute("gps-entity-place","latitude:" +latitude + "; longitude:" + longitude + ";");
-      console.log(`Latitude: ${latitude}, Longitude: ${longitude}`);
+      alert(`Latitude: ${latitude}, Longitude: ${longitude}`);
     });
   } else {
     console.log("Geolocation is not supported by this browser.");
